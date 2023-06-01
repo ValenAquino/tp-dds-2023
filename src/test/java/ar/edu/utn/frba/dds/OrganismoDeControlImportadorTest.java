a0a28d663f73042be48a5f48038e3c0434c61e82
mfmarano <mmarano@frba.utn.edu.ar>
2023-06-01 18:59:31 -0300

    fix(OrganismoDeControlImportadorTest): obtener path de archivo por URI

--- src/test/java/ar/edu/utn/frba/dds/OrganismoDeControlImportadorTest.java
+++ src/test/java/ar/edu/utn/frba/dds/OrganismoDeControlImportadorTest.java
@@ -2,32 +2,41 @@ package ar.edu.utn.frba.dds;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import ar.edu.utn.frba.dds.entidades.OrganismoDeControl;
 import ar.edu.utn.frba.dds.importadores.OrganismoDeControlImportador;
-import java.net.URL;
+import java.net.URISyntaxException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import java.nio.file.Paths;
 import java.util.List;
 
 public class OrganismoDeControlImportadorTest {
 
   private List<OrganismoDeControl> organismosDeControl;
+
   @BeforeEach
-  public void inicializarImportador() {
-    URL resourceUrl = getClass().getClassLoader().getResource("20organismos.csv");
-    assert resourceUrl != null;
-    OrganismoDeControlImportador importador = new OrganismoDeControlImportador(resourceUrl.getPath());
+  public void inicializarImportador() throws URISyntaxException {
+    var url = OrganismoDeControlImportadorTest.class
+        .getClassLoader()
+        .getResource("20organismos.csv");
+
+    assertNotNull(url);
+
+    OrganismoDeControlImportador importador = new OrganismoDeControlImportador(
+        Paths.get(url.toURI()).toString());
+
     organismosDeControl = importador.getOrganismosDeControl();
   }
 
   @Test
-  public void puedoObtener20OrganismosDeControl(){
+  public void puedoObtener20OrganismosDeControl() {
     assertNotNull(organismosDeControl);
     assertEquals(20, organismosDeControl.size());
   }
 
   @Test
-  public void seCarganBienLosNombres(){
+  public void seCarganBienLosNombres() {
     assertEquals("Organismo12", organismosDeControl.get(11).getNombre());
   }
 }
