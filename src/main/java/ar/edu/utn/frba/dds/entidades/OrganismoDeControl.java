57eb4bf4ee9df7a49147217474f1132e62c9f421
ValenAquino <vaquino@frba.utn.edu.ar>
2023-06-04 18:37:20 -0300

    feat: agregar validación en el constructor excepción

--- src/main/java/ar/edu/utn/frba/dds/entidades/OrganismoDeControl.java
+++ src/main/java/ar/edu/utn/frba/dds/entidades/OrganismoDeControl.java
@@ -1,5 +1,6 @@
 package ar.edu.utn.frba.dds.entidades;
 
+import ar.edu.utn.frba.dds.excepciones.OrganismoDeControlException;
 import java.util.List;
 
 public class OrganismoDeControl {
@@ -9,12 +10,27 @@ public class OrganismoDeControl {
   private List<Entidad> entidadesControladas;
 
   public OrganismoDeControl(String nombre, String correoElectronico) {
+    // TODO: Validar que los campos sean válidos
+    validarCampos(nombre, correoElectronico);
     this.nombre = nombre;
     this.correoElectronico = correoElectronico;
   }
 
+  private void validarCampos(String nombre, String correoElectronico) {
+    if (nombre == null || nombre.isEmpty()) {
+      throw new OrganismoDeControlException("El nombre no puede ser nulo o vacio");
+    }
+    if (correoElectronico == null || correoElectronico.isEmpty()) {
+      throw new OrganismoDeControlException("El correo electronico no puede ser nulo o vacio");
+    }
+  }
+
   public String getNombre() {
     return nombre;
+  }
+
+  public String getCorreoElectronico() {
+    return correoElectronico;
   }
 
   public void asignarResponsable(Usuario nuevoResponsable) {
