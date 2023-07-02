ee1f89f89a7d75ecdc62a5b72624ba99daa5a226
mfmarano <mmarano@frba.utn.edu.ar>
2023-07-02 02:06:57 -0300

    chore(servicio): agregar constructor

--- src/main/java/ar/edu/utn/frba/dds/entidades/Servicio.java
+++ src/main/java/ar/edu/utn/frba/dds/entidades/Servicio.java
@@ -3,4 +3,9 @@ package ar.edu.utn.frba.dds.entidades;
 public class Servicio {
   private String descripcion;
   private TipoDeServicio tipoDeServicio;
+
+  public Servicio(String descripcion, TipoDeServicio tipoDeServicio) {
+    this.descripcion = descripcion;
+    this.tipoDeServicio = tipoDeServicio;
+  }
 }
