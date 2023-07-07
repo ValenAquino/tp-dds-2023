7e5c91064d48f88dd4337e272806095e27dc6aa8
mfmarano <mmarano@frba.utn.edu.ar>
2023-07-07 20:38:43 -0300

    refactor(generador-csv)

--- src/main/java/ar/edu/utn/frba/dds/exportadores/GeneradorReportesCsv.java
+++ src/main/java/ar/edu/utn/frba/dds/exportadores/GeneradorReportesCsv.java
@@ -1,46 +1,48 @@
 package ar.edu.utn.frba.dds.exportadores;
 
-import ar.edu.utn.frba.dds.entidades.Entidad;
 import ar.edu.utn.frba.dds.entidades.rankings.Ranking;
 import ar.edu.utn.frba.dds.excepciones.GeneradorCsvException;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
-import java.util.Map;
 
 public class GeneradorReportesCsv {
-  private final String path = "./reportes/";
-  private Ranking ranking;
+  private String path = "./reportes/";
+  private final Ranking ranking;
 
   public GeneradorReportesCsv(Ranking ranking) {
     this.ranking = ranking;
   }
 
-  public void exportar() {
-    var nombre = "Ranking_" + this.ranking.getFecha().toString() + ".csv";
-    var entidades = this.ranking.getEntidades();
-    crearCsv(entidades, nombre);
+  public void setPath(String path) {
+    this.path = path;
   }
 
-  private void crearCsv(Map<Entidad, Double> entidades, String nombre) {
+  private String getNombreArchivo() {
+    return String.format(
+        "Ranking_%s_%s.csv",
+        ranking.getDescripcionCriterio(),
+        ranking.getFecha());
+  }
+
+  private void exportar() {
     try {
-      FileWriter fileWriter = new FileWriter(this.path + nombre);
+      FileWriter fileWriter = new FileWriter(path + getNombreArchivo());
       BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
 
       String header = "Posicion,Nombre,Valor";
       bufferedWriter.write(header);
       bufferedWriter.newLine();
 
-      Integer posicion = 1;
+      int posicion = 1;
 
-      for (var entry : entidades.entrySet()) {
+      for (var entry : ranking.getEntidades().entrySet()) {
         var entidad = entry.getKey();
         var valor = entry.getValue();
 
         var data = new String[]
-            { posicion.toString(), entidad.getNombre(), valor.toString() };
+            {Integer.toString(posicion), entidad.getNombre(), valor.toString()};
 
-        var dataLine =
-            String.join(",", data);
+        var dataLine = String.join(",", data);
 
         bufferedWriter.write(dataLine);
         bufferedWriter.newLine();
@@ -49,8 +51,7 @@ public class GeneradorReportesCsv {
       }
 
       bufferedWriter.close();
-    }
-    catch(Exception e) {
+    } catch (Exception e) {
       throw new GeneradorCsvException("Ocurrió un error al generar el archivo csv");
     }
   }
