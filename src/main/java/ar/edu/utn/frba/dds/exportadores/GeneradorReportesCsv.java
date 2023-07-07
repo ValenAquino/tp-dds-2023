356472cd72b18553e135d97ce94c4b7a5709fff7
Juan Larramendy <jlarramendy@frba.utn.edu.ar>
2023-07-07 15:12:18 -0300

    feat(csv_rankings): agregar creacion y escritura de csv

--- src/main/java/ar/edu/utn/frba/dds/exportadores/GeneradorReportesCsv.java
+++ src/main/java/ar/edu/utn/frba/dds/exportadores/GeneradorReportesCsv.java
@@ -1,9 +1,14 @@
 package ar.edu.utn.frba.dds.exportadores;
 
+import ar.edu.utn.frba.dds.entidades.Entidad;
 import ar.edu.utn.frba.dds.entidades.rankings.Ranking;
+import ar.edu.utn.frba.dds.excepciones.GeneradorCsvException;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.util.Map;
 
 public class GeneradorReportesCsv {
-  private final String path = "exports/";
+  private final String path = "./reportes/";
   private Ranking ranking;
 
   public GeneradorReportesCsv(Ranking ranking) {
@@ -11,5 +16,42 @@ public class GeneradorReportesCsv {
   }
 
   public void exportar() {
+    var nombre = "Ranking_" + this.ranking.getFecha().toString() + ".csv";
+    var entidades = this.ranking.getEntidades();
+    crearCsv(entidades, nombre);
+  }
+
+  private void crearCsv(Map<Entidad, Double> entidades, String nombre) {
+    try {
+      FileWriter fileWriter = new FileWriter(this.path + nombre);
+      BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+
+      String header = "Posicion,Nombre,Valor";
+      bufferedWriter.write(header);
+      bufferedWriter.newLine();
+
+      Integer posicion = 1;
+
+      for (var entry : entidades.entrySet()) {
+        var entidad = entry.getKey();
+        var valor = entry.getValue();
+
+        var data = new String[]
+            { posicion.toString(), entidad.getNombre(), valor.toString() };
+
+        var dataLine =
+            String.join(",", data);
+
+        bufferedWriter.write(dataLine);
+        bufferedWriter.newLine();
+
+        posicion++;
+      }
+
+      bufferedWriter.close();
+    }
+    catch(Exception e) {
+      throw new GeneradorCsvException("Ocurrió un error al generar el archivo csv");
+    }
   }
 }
