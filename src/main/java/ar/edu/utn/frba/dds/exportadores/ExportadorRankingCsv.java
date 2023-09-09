fccaa33c58d5eaeb1fa8a26b53b1d8b03ea9bf63
mfmarano <mmarano@frba.utn.edu.ar>
2023-09-09 18:50:55 -0300

    refactor(ExportadorRankingCsv): simplificar métodos y obtención de path

--- src/main/java/ar/edu/utn/frba/dds/exportadores/ExportadorRankingCsv.java
+++ src/main/java/ar/edu/utn/frba/dds/exportadores/ExportadorRankingCsv.java
@@ -1,19 +1,15 @@
 package ar.edu.utn.frba.dds.exportadores;
 
-import ar.edu.utn.frba.dds.entidades.Entidad;
 import ar.edu.utn.frba.dds.entidades.rankings.Ranking;
 import ar.edu.utn.frba.dds.excepciones.GeneradorCsvException;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVPrinter;
-
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
-import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class ExportadorRankingCsv {
   private final Ranking ranking;
@@ -24,58 +20,40 @@ public class ExportadorRankingCsv {
   }
 
   public String getNombreArchivo() {
-    DateTimeFormatter formato = DateTimeFormatter.ofPattern("yyyy_MM_dd");
-    String fechaFormateada = ranking.getFecha().format(formato);
-    String criterio = ranking.getDescripcionCriterio().replace(" ", "_");
-
     return String.format(
-        "Ranking_%s_%s.csv", criterio, fechaFormateada
+        "Ranking_%s_%s.csv",
+        ranking.getDescripcionCriterio().replace(" ", "_"),
+        ranking.getFecha().format(DateTimeFormatter.ofPattern("yyyy_MM_dd"))
     );
   }
 
   public void exportar() {
-    try {
-      CSVPrinter csvPrinter = generarCsvPrinter();
-
-      int puesto = 1;
-      for (Map.Entry<Entidad, Double> entry : ranking.getEntidades().entrySet()) {
-        Entidad entidad = entry.getKey();
-        Double valor = entry.getValue();
-        csvPrinter.printRecord(Arrays.asList(puesto, entidad.getNombre(), valor));
-        puesto++;
-      }
-
+    try (CSVPrinter csvPrinter = generarCsvPrinter()) {
+      AtomicInteger puesto = new AtomicInteger(1);
+      ranking.getEntidades().forEach((entidad, valor) ->
+          {
+            try {
+              csvPrinter.printRecord(puesto.getAndIncrement(), entidad.getNombre(), valor);
+            } catch (IOException e) {
+              throw new GeneradorCsvException("Error al escribir en el archivo CSV: " + e.getMessage());
+            }
+          }
+      );
       csvPrinter.flush();
     } catch (Exception e) {
-      throw new GeneradorCsvException("Fallo la generacion del csv: " + e.getMessage());
+      throw new GeneradorCsvException("Falló la generación del archivo CSV: " + e.getMessage());
     }
   }
 
   private CSVPrinter generarCsvPrinter() throws IOException {
-    String rutaCompleta = this.construirRutaCompleta();
-    BufferedWriter writer = Files.newBufferedWriter(Paths.get(rutaCompleta));
-    CSVFormat csvFormat = CSVFormat.Builder.create().setHeader(header).build();
-    return new CSVPrinter(writer, csvFormat);
-  }
+    Path carpetaReportesPath = Paths.get("", "reportes");
+    Files.createDirectories(carpetaReportesPath);
 
-  private String construirRutaCompleta() throws IOException {
-    String pathDirectorio = this.crearDirectorioReportesSiNoExiste();
-    String nombreArchivo = this.getNombreArchivo();
-
-    System.out.println(nombreArchivo);
-
-    return pathDirectorio + nombreArchivo;
-  }
-
-  private String crearDirectorioReportesSiNoExiste() throws IOException {
-    String subDirectorio = "/reportes/";
-    String carpetaReportes = Paths.get("").toAbsolutePath() + subDirectorio;
-    Path carpetaReportesPath = Paths.get(carpetaReportes);
-
-    if (!Files.exists(carpetaReportesPath)) {
-      Files.createDirectories(carpetaReportesPath);
-    }
-
-    return carpetaReportes;
+    return new CSVPrinter(
+        Files.newBufferedWriter(
+            Paths.get(carpetaReportesPath.resolve(getNombreArchivo()).toString())
+        ),
+        CSVFormat.Builder.create().setHeader(header).build()
+    );
   }
 }
