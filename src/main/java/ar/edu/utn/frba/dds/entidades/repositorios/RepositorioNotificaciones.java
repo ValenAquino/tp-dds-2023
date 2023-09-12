a2231c47713c64869abf0e5ebfa17eedf2ce1075
matirodriguez27 <82338146+matirodriguez27@users.noreply.github.com>
2023-09-12 19:40:43 -0300

    feat(jpa): RepositorioNotificaciones

--- src/main/java/ar/edu/utn/frba/dds/entidades/repositorios/RepositorioNotificaciones.java
+++ src/main/java/ar/edu/utn/frba/dds/entidades/repositorios/RepositorioNotificaciones.java
@@ -1,28 +1,27 @@
 package ar.edu.utn.frba.dds.entidades.repositorios;
 
 import ar.edu.utn.frba.dds.notificaciones.Notificacion;
+import io.github.flbulgarelli.jpa.extras.simple.WithSimplePersistenceUnit;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class RepositorioNotificaciones {
+public class RepositorioNotificaciones implements WithSimplePersistenceUnit {
   private static RepositorioNotificaciones instance;
   private final List<Notificacion> notificaciones = new ArrayList<>();
-
+  @SuppressWarnings("unchecked")
   public List<Notificacion> todas() {
-    return this.notificaciones;
+    return entityManager().createQuery("from Notificacion").getResultList();
   }
-
-  public static RepositorioNotificaciones getInstance() {
-    if (instance == null) {
-      instance = new RepositorioNotificaciones();
-    }
-    return instance;
-  }
-
+  @SuppressWarnings("unchecked")
   public List<Notificacion> notificacionesPendientes() {
     return todas().stream()
         .filter(n -> !n.fueEnviada())
         .collect(Collectors.toList());
+
+    // Con un boolean enviada la consulta podría ser
+    // return entityManager()
+    //        .createQuery("from Notificacion n where n.enviada = false")
+    //        .getResultList();
   }
 }
