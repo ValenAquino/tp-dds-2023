cfd993483ca56e5dfb6a0430766a2e4537fc4507
matirodriguez27 <82338146+matirodriguez27@users.noreply.github.com>
2023-09-12 18:48:34 -0300

    feat(jpa): mapear NotificacionRevisionIncidente

--- src/main/java/ar/edu/utn/frba/dds/notificaciones/NotificacionRevisionIncidente.java
+++ src/main/java/ar/edu/utn/frba/dds/notificaciones/NotificacionRevisionIncidente.java
@@ -2,11 +2,14 @@ package ar.edu.utn.frba.dds.notificaciones;
 
 import ar.edu.utn.frba.dds.entidades.Incidente;
 import ar.edu.utn.frba.dds.entidades.Usuario;
+import org.hibernate.annotations.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 public class NotificacionRevisionIncidente extends Notificacion {
-
+  @ManyToOne
+  @JoinColumn(name = "incidente_id")
   Incidente incidente;
-
   public NotificacionRevisionIncidente(Usuario receptor, Incidente incidente) {
     super(receptor);
     this.incidente = incidente;
@@ -25,7 +28,6 @@ public class NotificacionRevisionIncidente extends Notificacion {
         incidente.getObservaciones()
     );
   }
-
   public Incidente getIncidente() {
     return incidente;
   }
