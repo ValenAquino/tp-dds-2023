43d43ce32d6bf9fc3f1afb407f0cc909814d93ad
mfmarano <mmarano@frba.utn.edu.ar>
2023-09-15 22:43:29 -0300

    refactor(notificaciones-pendientes): ejecutar main en transacción

--- src/main/java/ar/edu/utn/frba/dds/NotificacionesPendientes.java
+++ src/main/java/ar/edu/utn/frba/dds/NotificacionesPendientes.java
@@ -2,11 +2,17 @@ package ar.edu.utn.frba.dds;
 
 import ar.edu.utn.frba.dds.entidades.repositorios.RepositorioNotificaciones;
 import ar.edu.utn.frba.dds.notificaciones.Notificacion;
+import io.github.flbulgarelli.jpa.extras.simple.WithSimplePersistenceUnit;
 
-public class NotificacionesPendientes {
+public class NotificacionesPendientes implements WithSimplePersistenceUnit {
 
   public static void main(String[] args) {
-    RepositorioNotificaciones repo = new RepositorioNotificaciones();
-    repo.notificacionesPendientes().forEach(Notificacion::enviar);
+    new NotificacionesPendientes().notificarPendientes();
+  }
+
+  public void notificarPendientes() {
+    withTransaction(() -> new RepositorioNotificaciones()
+        .notificacionesPendientes()
+        .forEach(Notificacion::enviar));
   }
 }
