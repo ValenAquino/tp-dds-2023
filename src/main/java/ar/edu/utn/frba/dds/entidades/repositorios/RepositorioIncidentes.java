b7dc2b8aa90742c17f920e94805d7cb0dbf464da
matirodriguez27 <82338146+matirodriguez27@users.noreply.github.com>
2023-10-25 18:36:27 -0300

    feat(RepositorioIncidentes): agregar consultas

--- src/main/java/ar/edu/utn/frba/dds/entidades/repositorios/RepositorioIncidentes.java
+++ src/main/java/ar/edu/utn/frba/dds/entidades/repositorios/RepositorioIncidentes.java
@@ -1,6 +1,9 @@
 package ar.edu.utn.frba.dds.entidades.repositorios;
 
 import ar.edu.utn.frba.dds.entidades.Incidente;
+import ar.edu.utn.frba.dds.entidades.Servicio;
+import ar.edu.utn.frba.dds.entidades.Usuario;
+import ar.edu.utn.frba.dds.notificaciones.Notificacion;
 import io.github.flbulgarelli.jpa.extras.simple.WithSimplePersistenceUnit;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -30,11 +33,6 @@ public class RepositorioIncidentes implements WithSimplePersistenceUnit {
     }
   }
 
-  @SuppressWarnings("unchecked")
-  public List<Incidente> todos() {
-    return entityManager().createQuery("from incidentes").getResultList();
-  }
-
   public List<Incidente> ultimaSemana() {
     LocalDateTime ahora = LocalDateTime.now();
     LocalDateTime fechaLimite = ahora.minusDays(7);
@@ -42,7 +40,28 @@ public class RepositorioIncidentes implements WithSimplePersistenceUnit {
     return todos().stream()
         .filter(i -> i.getFecha().isAfter(fechaLimite) && i.getFecha().isBefore(ahora))
         .collect(Collectors.toList());
+  }
 
-    // TODO: Implementar con query --> Borrar todos()
+  public List<Incidente> todos() {
+    return entityManager().createQuery("SELECT i FROM Incidente", Incidente.class)
+        .getResultList();
+  }
+  public List<Incidente> incidentesResueltos() {
+    return entityManager().createQuery("SELECT i FROM Incidente i WHERE i.resuelto = true", Incidente.class)
+        .getResultList();
+  }
+  public List<Incidente> incidentesNoResueltos() {
+    return  entityManager().createQuery("SELECT i FROM Incidente i WHERE i.resuelto = false", Incidente.class)
+        .getResultList();
+  }
+  public List<Incidente> incidentesDelServicio(Servicio servicio) {
+    return  entityManager().createQuery("SELECT i FROM Incidente i WHERE i.servicio = :servicio", Incidente.class)
+        .setParameter("servicio", servicio)
+        .getResultList();
+  }
+  public List<Incidente> incidentesDelReportante(Usuario usuario) {
+    return  entityManager().createQuery("SELECT i FROM Incidente i WHERE i.reportante = :usuario", Incidente.class)
+        .setParameter("usuario", usuario)
+        .getResultList();
   }
 }
