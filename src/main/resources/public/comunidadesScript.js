35c44d2e653c8afbaa5e50b26b9d89f61ae7da26
Juan Larramendy <jlarramendy@frba.utn.edu.ar>
2023-11-24 13:45:49 -0300

    feat(css): agregar modales para delete, centrar columnas de tablas

---
+++ src/main/resources/public/comunidadesScript.js
@@ -0,0 +1,14 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    const closeButton = document.querySelector('[data-bs-target="#confirmarEliminarModal"]');
+    const form = document.querySelector('#confirmarEliminarModal form');
+
+    document.addEventListener('click', function (event) {
+      if (event.target && event.target.getAttribute('data-comunidad-id')) {
+        const comunidadId = event.target.getAttribute('data-comunidad-id');
+
+        //form.method = "DELETE";
+        form.action = `comunidades/${comunidadId}`;
+      }
+    });
+});
---
+++ src/main/resources/public/usuariosScript.js
@@ -0,0 +1,14 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    const closeButton = document.querySelector('[data-bs-target="#confirmarEliminarModal"]');
+    const form = document.querySelector('#confirmarEliminarModal form');
+
+    document.addEventListener('click', function (event) {
+      if (event.target && event.target.getAttribute('data-usuario-id')) {
+        const usuarioId = event.target.getAttribute('data-usuario-id');
+
+        //form.method = "DELETE";
+        form.action = `usuarios/${usuarioId}`;
+      }
+    });
+});
